ec9b52b964b5954dab513b67a8e168516e54a040
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2017-06-11 00:33:46 -0400

    Add first post

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { Router, Route, browserHistory } from "react-router"
-import { createApp } from "@phenomic/preset-react-app/lib/client"
+import { createApp, createContainer, query, BodyRenderer } from "@phenomic/preset-react-app/lib/client"
 
 const Home = () => (
   <div>
@@ -8,8 +8,24 @@ const Home = () => (
   </div>
 )
 
+const BlogPost = ({ page }) => (
+  <div>
+    {page.node && (
+      <article>
+        <h1>{ page.node.title }</h1>
+        <BodyRenderer>{ page.node.body }</BodyRenderer>
+      </article>
+    )}
+  </div>
+)
+
+const BlogPostContainer = createContainer(BlogPost, (props) => ({
+  page: query({ collection: "posts", id: props.params.splat }),
+}))
+
 export default createApp(() => (
   <Router history={ browserHistory }>
     <Route path="/" component={ Home } />
+    <Route path="/blog/*" component={ BlogPostContainer } collection="posts" />
   </Router>
 ))
