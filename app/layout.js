c2a6f3f56512fde690fac2d1f4ff775dbc797e58
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2017-06-12 23:36:22 -0400

    Adding in React bootstrap

--- app/layout.js
+++ app/layout.js
@@ -1,16 +1,40 @@
 import React from "react"
+import { Nav, Navbar, NavItem, MenuItem, NavDropdown, Grid } from "react-bootstrap"
+
+const NavbarInstance = () => (
+  <Navbar>
+    <Navbar.Header>
+      <Navbar.Brand>
+        <a href="#">React-Bootstrap</a>
+      </Navbar.Brand>
+    </Navbar.Header>
+    <Nav>
+      <NavItem eventKey={1} href="#">Link</NavItem>
+      <NavItem eventKey={2} href="#">Link</NavItem>
+      <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+        <MenuItem eventKey={3.1}>Action</MenuItem>
+        <MenuItem eventKey={3.2}>Another action</MenuItem>
+        <MenuItem eventKey={3.3}>Something else here</MenuItem>
+        <MenuItem divider />
+        <MenuItem eventKey={3.4}>Separated link</MenuItem>
+      </NavDropdown>
+    </Nav>
+  </Navbar>
+);
 
 export const Layout = ({children}) => (
   <div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/react-bootstrap/latest/react-bootstrap.min.js"></script>
     <div class="container">
-      <header>
-        <p>Header</p>
-      </header>
-      <div>{ children }</div>
-      <footer>
-        <p>Footer</p>
-      </footer>
-      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap.min.css" />
+      <NavbarInstance/>
+      <Grid>
+        <div>{ children }</div>
+        <footer>
+          <p>Footer</p>
+        </footer>
+      </Grid>
     </div>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap-theme.min.css" />
   </div>
 )
